7ce2f3e8b80bade170ac746e1f9b389905c934c6
Andrew Whitley <awhitley1233@gmail.com>
2017-05-26 21:13:29 -0400

    finished program create / validations

--- exit_ticket_app/app/controllers/programs_controller.rb
+++ exit_ticket_app/app/controllers/programs_controller.rb
@@ -38,7 +38,12 @@ class ProgramsController < ApplicationController
   end
 
   def create
-
+    @program = Program.new(program_params)
+    if @program.save
+      redirect_to program_path(@program)
+    else
+      render :new
+    end
   end
 
   private
--- exit_ticket_app/app/models/program.rb
+++ exit_ticket_app/app/models/program.rb
@@ -2,4 +2,7 @@ class Program < ApplicationRecord
   has_many :memberships
   has_many :users, through: :memberships
   has_many :lessons
+
+  validates :name, presence: true
+  validates :location, presence: true
 end
